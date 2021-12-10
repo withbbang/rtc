896468dc5128d19201ba2c60c7fbda60fb09e93b
unknown <withbbang@gmail.com>
2021-12-10 17:21:24 +0900

    update

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 5000;
 const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid");
-const io = require("socket.io")(server);
+app.set("view engine", "ejs");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
 const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
   debug: true,
@@ -11,7 +15,6 @@ const peerServer = ExpressPeerServer(server, {
 
 app.use("/peerjs", peerServer);
 app.use(express.static("public"));
-app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.redirect(`/${uuidv4()}`);
@@ -22,12 +25,13 @@ app.get("/:room", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  socket.on("join-room", (roomId, userId) => {
+  socket.on("join-room", (roomId, userId, userName) => {
     socket.join(roomId);
     socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message, userName);
+    });
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`server running on ${PORT}`);
-});
+server.listen(process.env.PORT || 3030);
